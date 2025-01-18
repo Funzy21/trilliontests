b83402f2fbe6a06aad02756703ebffc438bb6036
Johannes <zberzerker246@gmail.com>
2025-01-19 02:25:51 +0800

    added a few more preferences

--- app/src/main/java/com/trilliontests/model/userdata/UserPreferences.kt
+++ app/src/main/java/com/trilliontests/model/userdata/UserPreferences.kt
@@ -1,7 +1,15 @@
 package com.trilliontests.model.userdata
 
 data class UserPreferences(
+    // Initially set during onboarding
     val hasCompletedOnboarding: Boolean = false,
     val userType: String? = null,
-    val studyPurposes: Set<String> = emptySet()
+    val studyPurposes: Set<String> = emptySet(),
+
+    // Manually toggled in settings
+    val isDarkMode: Boolean,
+    val language: String,
+
+    // Data set automatically
+    val recentActivity: List<String>
 ) 
