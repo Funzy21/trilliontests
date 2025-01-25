8d2c856aa9c25b1d16396955d6b4c822c727ee73
Gil Joshua Asingua <giljoshuaasingua@gmail.com>
2025-01-25 09:19:24 +0800

    Added user preferences data such as isDarkMode, language, and recentActivity

--- app/src/main/java/com/trilliontests/data/repository/PreferencesRepository.kt
+++ app/src/main/java/com/trilliontests/data/repository/PreferencesRepository.kt
@@ -1,15 +1,20 @@
 package com.trilliontests.data.repository
 
 // TODO: THIS REPOSITORY WILL STORE USER PREFERENCE DATA
-/*
 
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import com.trilliontests.model.userdata.UserPreferences
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.first
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -23,13 +28,19 @@ class PreferencesRepository @Inject constructor(
         val HAS_COMPLETED_ONBOARDING = booleanPreferencesKey("has_completed_onboarding")
         val USER_TYPE = stringPreferencesKey("user_type")
         val STUDY_PURPOSES = stringSetPreferencesKey("study_purposes")
+        val IS_DARK_MODE = booleanPreferencesKey("isDarkMode")
+        val LANGUAGE = stringPreferencesKey("language")
+        val RECENT_ACTIVITY = stringPreferencesKey("recentActivity")
     }
 
     val userPreferences: Flow<UserPreferences> = context.dataStore.data.map { preferences ->
         UserPreferences(
             hasCompletedOnboarding = preferences[PreferencesKeys.HAS_COMPLETED_ONBOARDING] ?: false,
             userType = preferences[PreferencesKeys.USER_TYPE],
-            studyPurposes = preferences[PreferencesKeys.STUDY_PURPOSES] ?: emptySet()
+            studyPurposes = preferences[PreferencesKeys.STUDY_PURPOSES] ?: emptySet(),
+            isDarkMode = preferences[PreferencesKeys.IS_DARK_MODE] ?: false,
+            language = preferences[PreferencesKeys.LANGUAGE],
+            recentActivity = preferences[PreferencesKeys.RECENT_ACTIVITY],
         )
     }
 
@@ -50,6 +61,17 @@ class PreferencesRepository @Inject constructor(
             preferences[PreferencesKeys.STUDY_PURPOSES] = purposes
         }
     }
-}
 
-*/
+    suspend fun updateIsDarkMode(darkMode: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[PreferencesKeys.IS_DARK_MODE] = darkMode
+        }
+    }
+
+    suspend fun updateRecentActivity(recentActivity: List<String>){
+        val jsonString = Json.encodeToString(recentActivity) // Serialize recentActivity to JSON
+        context.dataStore.edit { preferences ->
+            preferences[PreferencesKeys.RECENT_ACTIVITY] = jsonString
+        }
+    }
+}
